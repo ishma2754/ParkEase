fe7a5d27d7af324bb67bdc49b89295de255acaba
ishma2754 <ishmakhan1995@gmail.com>
2024-09-30 19:03:09 +0530

    changed the layout of bookings page

--- src/components/DisplayPark.jsx
+++ src/components/DisplayPark.jsx
@@ -1,10 +1,12 @@
+import {useNavigate } from "react-router-dom";
 import { ParkCard } from "./index";
 
 export default function DisplayPark({ distance, duration, park }) {
+  const navigate = useNavigate()
   if (!distance.text && !duration) return null;
 
   return (
-    <div className="flex flex-col p-2 bg-gray-800 rounded-lg shadow-md transition-shadow duration-300 hover:shadow-lg border-8 border-orange-600">
+    <div className="flex flex-col p-4 bg-gray-800 rounded-lg shadow-md transition-shadow duration-300 hover:shadow-lg border-8 border-orange-600">
       <ParkCard
         park={park}
         distance={distance.text}
--- src/components/ParkCard.jsx
+++ src/components/ParkCard.jsx
@@ -15,16 +15,18 @@ const ParkCard = ({
       onClick={onClick}
     >
       <figure className="relative">
-        <img src={park.image_url} alt={park.name} className={`${imageClassName}}`} />
+        <img
+          src={park.image_url}
+          alt={park.name}
+          className={`${imageClassName}`}
+        />
         <div className="absolute top-1 left-1 bg-gray-800 text-white px-2 py-1 rounded font-semibold font-poppins tracking-wide">
           Rs {park.price_per_hour} / hr
         </div>
 
-        {onClick && (
-          <button className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-500 text-white px-4 py-1 rounded-md font-semibold transition-colors duration-300 hover:bg-orange-600 z-10">
-            Click Here
-          </button>
-        )}
+        <button className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-500 text-white px-4 py-1 rounded-md font-semibold transition-colors duration-300 hover:bg-orange-600 z-10">
+          Click Here
+        </button>
       </figure>
       <div className="p-2 text-center">
         <h2 className="text-gray-200 font-bold tracking-wide mb-1">
@@ -35,7 +37,7 @@ const ParkCard = ({
         </p>
         {distance && duration && (
           <p className="text-sm font-bold">
-            Distance: {distance.text} | Duration: {duration}
+            Distance: {distance} | Duration: {duration}
           </p>
         )}
       </div>
